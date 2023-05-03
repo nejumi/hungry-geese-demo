0f64589f765fe055d0c06a9f288ccb6cebf3b4e7
YuyaYAMAMOTO <dr_jingles@mac.com>
2023-05-04 00:52:52 +0900

    create input

--- src/get_data.py
+++ src/get_data.py
@@ -2,4 +2,7 @@
 # coding: utf-8
 
 import opendatasets as od
+from utils import create_folders_if_not_exist
+
+create_folders_if_not_exist(['../input'])
 od.download('https://www.kaggle.com/datasets/nejumi/hungry-geese-episodes-created-by-self-play', data_dir='../input/')
